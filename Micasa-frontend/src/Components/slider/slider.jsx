0d3f185598c60e9ef0416913416b3a3cf7c84711
Greta-Alfredsdottir <eldvakinn@hotmail.com>
2026-08-13 08:12:24 +0200

    slider

--- Micasa-frontend/src/Components/slider/slider.jsx
+++ Micasa-frontend/src/Components/slider/slider.jsx
@@ -4,14 +4,30 @@ import sliderImage2 from '../../assets/sliderImages/apartment-2.jpg'
 import sliderImage3 from '../../assets/sliderImages/apartment-3.jpg'
 
 export function Slider(){
-    const [sliderIndex, setSliderIndex] = useState
+    const [sliderIndex, setSliderIndex] = useState(0);
     // Billeder skal i et array
     // Back and forth knap
     const sliderImages =[sliderImage1,sliderImage2,sliderImage3];
+    
+    function forward(){
+        if (sliderIndex >= sliderImages, length){
+            setSliderIndex(0)
+        }   else setSliderIndex(prev => prev + 1)
+    }
+
+    function back(){
+        if (sliderIndex === 0){
+            setSliderIndex(sæiderImages.length -1)
+        }   else setSliderIndex((prev) => prev -1)
+    }
 
     return(
         <figure>
             <img src={sliderImages[sliderIndex]} />
+            <figurecaption>
+            <button onClick={() => back()}>Previous</button>
+            <button onClick={() => forward()}>Next</button>
+            </figurecaption>
         </figure>
     )
 }
