ecb217d8e1c7314015ab4d719c239a6e7efbe063
Greta-Alfredsdottir <eldvakinn@hotmail.com>
2026-08-13 11:03:24 +0200

    startet authtoken

--- Micasa-frontend/src/Pages/Loginpage.jsx
+++ Micasa-frontend/src/Pages/Loginpage.jsx
@@ -1,6 +1,27 @@
 export function Loginpage(){
+    const Login = async (e) => {
+        e.preventDefault();
+        const username = e.target.username.value;
+        const password = e.target.password.value;
+
+        const bodyJson = strinify({
+            username: username,
+            password: password,
+        })
+        const res = await fetch("http://localhost")
+
+    } 
 
     return(
      <h1>Loginpage</h1>   
     )
 }
+
+
+
+
+
+
+
+
+
---
+++ Micasa-frontend/src/context/AuthContext.jsx
@@ -0,0 +1,12 @@
+import { createContext, useState } from "react";
+
+
+const [authToken, setAuthToken] = useState(null)
+
+export const AuthContextProvider = ({children}) => {
+    return{
+        <AuthContext.Provider value={authToken, setAuthToken}}>
+        {children}
+        </AuthContext.Provider>
+    }
+}
